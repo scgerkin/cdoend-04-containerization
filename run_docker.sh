227e3a3951df18dc7818d6c0df540364cb803cb5
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-07 04:43:07 +0000

    Implement run_docker script

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
+# container name and image tag
+cname="prediction"
+ctag="prediction_app"
+
+# container port mappings
+inPort=80
+outPort=8000
+
+# for distictive output
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[1;31m'
+NC='\033[0m'
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=$ctag --rm .
 
-# Step 2: 
+# Step 2:
 # List docker images
+echo -e "${GREEN}Listing recent images...${NC}"
+docker image ls
 
-# Step 3: 
+# Step 3:
 # Run flask app
+docker run -p $outPort:$inPort -d --name $cname $ctag
+
+# get the truncated container ID
+cid=$(docker ps -qf "name=$cname")
+
+echo -e "
+${GREEN}Running container in detached state...
+${RED}Image Tag: ${BLUE}$ctag
+${RED}Container Name: ${BLUE}$cname
+${RED}Container ID: ${BLUE}$cid
+${GREEN}Access the application on port: ${BLUE}$outPort${NC}
+"
