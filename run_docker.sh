a55fec9c2570375c3f2a8fb91671b48bd3330968
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-08 16:39:48 +0000

    Final fix run_docker script.
    Now only does as required by project.
    Deletes existing container if already exists to keep
    the system clean, but leaves the newly created container
    intact so it can be used by other scripts or by running
    the container directly.

--- run_docker.sh
+++ run_docker.sh
@@ -1,63 +1,19 @@
-#!/usr/bin/env bash
+imageTag="prediction_image"
+containerName="prediction_container"
+hostPort="8000"
+appPort="80"
 
-# container name and image tag
-cname="prediction"
-ctag="prediction_app"
+# Remove existing container if exists
+docker rm $containerName
 
-# container port mappings
-inPort=80
-outPort=8000
+# Build image
+docker build --tag=$imageTag --rm .
 
-# for distictive output
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-RED='\033[1;31m'
-NC='\033[0m'
+# List images
+docker images
 
-# Step 1:
-# Build image and add a descriptive tag
-echo -e "${GREEN}Building a fresh image...${NC}"
-docker build --tag=$ctag --rm .
-
-# Step 2:
-# List docker images
-echo -e "${GREEN}Listing recent images...${NC}"
-docker image ls
-
-# Step 3:
-# Run flask app
-echo -e "${GREEN}Starting container...${NC}"
-docker run -p $outPort:$inPort -d --name $cname $ctag
-
-# get the truncated container ID
-cid=$(docker ps -qf "name=$cname")
-
-echo -e "
-${GREEN}The ${BLUE}$cname${GREEN} container has started in detached mode.
-${RED}Image Tag: ${BLUE}$ctag
-${RED}Container Name: ${BLUE}$cname
-${RED}Container ID: ${BLUE}$cid
-${RED}Application port: ${BLUE}$outPort${NC}
-
-${GREEN}You can now post predictions to the service.
-${GREEN}Run ${BLUE}make_prediction.sh ${GREEN}for a sample output.
-
-The container will now be attached to STDIN.
-${RED}Stop the container with ${BLUE}CTRL+C${NC}
-"
-
-docker attach $cname
-
-echo -e "
-${RED}Container Stopped!
-Doing some clean up...${NC}"
-
-# remove container
-removedContainer=$(docker container rm $cname)
-# remove image
-docker image rm $ctag
-
-echo -e "${GREEN}The container '${BLUE}$removedContainer${GREEN}' and \
-associated images have been ${RED}removed.
-${GREEN}Restart the container by running ${BLUE}run_docker.sh${GREEN} again.
-${NC}"
+# Run app
+echo "Starting container with ID:"
+docker run -d -p $hostPort:$appPort --name $containerName $imageTag
+echo "Attaching to container."
+docker attach $containerName
