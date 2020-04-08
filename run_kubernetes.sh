202c8506986d51c2a4cccaf40522eb4b235ee11d
Stephen Gerkin <stephen.gerkin@gmail.com>
2020-04-08 16:05:06 -0400

    Implement run_kubernetes script.
    A PowerShell implementation has been included for
    running on a Windows machine that allows virtualization.
    The Linux machine used for the first part of development
    did not allow for virtualization, so the project was finished
    in a Windows environment.

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="scgerkin/prediction-app"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prediction 8000:80
